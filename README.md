1d1db0ff90e4ff1d2fdb50af410415417050106c
Roman Yurchak <rth.yurchak@gmail.com>
2023-06-22 15:08:20 +0200

    Add pyodide-lock.json spec

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # pyodide-utils
-A set of shared utils in the Pyodide ecosystem
+Shared utils for the Pyodide ecosystem
+
+## Installation
+
+```bash
+pip install pyodide-utils
+```
+
+
+## Functionality
+
+1. Parsing and writing the `pyodide-lock.json` (formerly `repodata.json`) file:
+```py
+from pyodide_utils.lock_spec import PyodideLockSpec
+
+lock_spec = PyodideLockSpec.from_file("pyodide-lock.json")
+# Make some changes
+lock_spec.to_file("pyodide-lock.json")
+```
+
+## License
+
+BSD-3-Clause License
---
+++ tests/data/test_lock_spec.py
@@ -0,0 +1,30 @@
+import gzip
+import shutil
+from pathlib import Path
+
+import pytest
+
+from pyodide_utils.lock_spec import PyodideLockSpec
+
+DATA_DIR = Path(__file__).parents[1] / "data"
+
+
+@pytest.mark.parametrize("pyodide_version", ["0.23.3"])
+def test_lock_spec_parsing(pyodide_version, tmp_path):
+    source_path = DATA_DIR / f"pyodide-lock-{pyodide_version}.json.gz"
+    target_path = tmp_path / "pyodide-lock.json"
+    target2_path = tmp_path / "pyodide-lock2.json"
+
+    with gzip.open(source_path) as fh_in:
+        with target_path.open("wb") as fh_out:
+            shutil.copyfileobj(fh_in, fh_out)
+
+    spec = PyodideLockSpec.from_json(target_path)
+    spec.to_json(target2_path, indent=2)
+
+    spec2 = PyodideLockSpec.from_json(target2_path)
+
+    assert spec.info == spec2.info
+    assert set(spec.packages.keys()) == set(spec2.packages.keys())
+    for key in spec.packages:
+        assert spec.packages[key] == spec2.packages[key]
