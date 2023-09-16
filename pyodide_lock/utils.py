1658265ac4b791ed34e6eab1162235d7016381cb
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-09-15 21:01:18 -0500

    port parse_top_level_import_name

--- pyodide_lock/utils.py
+++ pyodide_lock/utils.py
@@ -1,5 +1,58 @@
 import hashlib
+import logging
+import zipfile
+from collections import deque
 from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+def parse_top_level_import_name(whlfile: Path) -> list[str] | None:
+    """
+    Parse the top-level import names from a wheel file.
+    """
+
+    if not whlfile.name.endswith(".whl"):
+        raise RuntimeError(f"{whlfile} is not a wheel file.")
+
+    whlzip = zipfile.Path(whlfile)
+
+    # If there is no top_level.txt file, we will find top level imports by
+    # 1) a python file on a top-level directory
+    # 2) a sub directory with __init__.py
+    # following: https://github.com/pypa/setuptools/blob/d680efc8b4cd9aa388d07d3e298b870d26e9e04b/setuptools/discovery.py#L122
+    top_level_imports = []
+    for subdir in whlzip.iterdir():
+        if subdir.is_file() and subdir.name.endswith(".py"):
+            top_level_imports.append(subdir.name[:-3])
+        elif subdir.is_dir() and _valid_package_name(subdir.name):
+            if _has_python_file(subdir):
+                top_level_imports.append(subdir.name)
+
+    if not top_level_imports:
+        logger.warning(
+            f"WARNING: failed to parse top level import name from {whlfile}."
+        )
+        return None
+
+    return top_level_imports
+
+
+def _valid_package_name(dirname: str) -> bool:
+    return all([invalid_chr not in dirname for invalid_chr in ".- "])
+
+
+def _has_python_file(subdir: zipfile.Path) -> bool:
+    queue = deque([subdir])
+    while queue:
+        nested_subdir = queue.pop()
+        for subfile in nested_subdir.iterdir():
+            if subfile.is_file() and subfile.name.endswith(".py"):
+                return True
+            elif subfile.is_dir() and _valid_package_name(subfile.name):
+                queue.append(subfile)
+
+    return False
 
 
 def _generate_package_hash(full_path: Path) -> str:
