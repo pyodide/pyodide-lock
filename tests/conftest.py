c109fbe4795293ecc9d4c528c4b14484ee330456
Gyeongjae Choi <def6488@gmail.com>
2024-04-03 11:29:10 +0000

    Remove temporary fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,6 @@ import build
 import pytest
 from packaging.utils import canonicalize_name
 
-import sys
-sys.path.insert(0, str(Path(__file__).parent.parent))
-
 from pyodide_lock import PyodideLockSpec
 from pyodide_lock.utils import _get_marker_environment
 
